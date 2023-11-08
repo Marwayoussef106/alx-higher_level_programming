e0600d62e98fd6437bd1d54acb11cfe233356775
Marwayoussef106 <marwaadel713@gmail.com>
2023-11-08 08:04:49 -0800

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+def uniq_add(my_list=[]):
+    number = 0
+    for element in set(my_list):
+        number += element
+    return number
